f6bc076620959310662bd7e3c97079559d07f607
餅乾 菜菜菜 <kola-319@hotmail.com>
2022-03-18 14:18:15 +0800

    test

---
+++ webapp/src/common/index.ts
@@ -0,0 +1,15 @@
+﻿import { createApp } from "vue";
+import App from "../app-components/App.vue";
+
+const app = createApp({
+    data() {
+        return {
+            message: 'Hello Vue!'
+        }
+    }
+}).mount('#app');
+
+function TSButton() {
+    let name: string = "Fred";
+    document.getElementById("ts-example").innerHTML = "abcde";
+}
