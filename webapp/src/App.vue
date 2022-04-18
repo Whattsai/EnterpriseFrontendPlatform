025ae47627dd6c43b4512ab1ef8b8f4e00e761d4
葉芃 <lamcaleter@gmail.com>
2022-04-18 17:47:08 +0800

    調整VUE

--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -1,6 +1,7 @@
 ﻿<template>
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+
 </template>
 
 
@@ -16,6 +17,7 @@
     import './css/style.css';
     import './css/YearEndBonusesreset.css';
     import './css//YearEndBonusesstyle.css';
+    import axios from 'axios';
 
     @Options({
         components: {
@@ -24,6 +26,19 @@
     })
     export default class App extends Vue {
 
+        mounted() {
+            this.Aggr_Post();
+        }
+
+        Aggr_Post() {
+            console.log('POST');
+            axios({
+                method: 'post',
+                url: 'https://hexschool-tutorial.herokuapp.com/api/signup',
+                //API要求的資料
+                data: 'A:"A"',
+            }).then((response) => console.log(response.data))
+        }
     }
 </script>
 
